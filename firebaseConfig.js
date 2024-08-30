48d126c3ad1b935bf0e4c2dbd1ceed673ed6c4f3
HussamK <hussamapt@gmail.com>
2024-08-29 19:50:40 -0500

    fixed some packages, added auth persistance

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,9 +1,21 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, signOut } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
-import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID } from '@env'
-
+import {
+  getAuth,
+  signOut,
+  initializeAuth,
+  getReactNativePersistence,
+} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+} from "@env";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
   apiKey: API_KEY,
@@ -11,18 +23,22 @@ const firebaseConfig = {
   projectId: PROJECT_ID,
   storageBucket: STORAGE_BUCKET,
   messagingSenderId: MESSAGING_SENDER_ID,
-  appId: APP_ID
+  appId: APP_ID,
 };
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
+// Initialize Firebase Auth with persistence
+export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
 export const FIRESTORE_DB = getFirestore(FIREBASE_APP);
-export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
-export const FIREBASE_STORAGE = getStorage()
+// export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+export const FIREBASE_STORAGE = getStorage();
 export const signOutUser = async () => {
   try {
     await signOut(FIREBASE_AUTH);
   } catch (error) {
     console.error("Error signing out: ", error);
   }
-}
+};
